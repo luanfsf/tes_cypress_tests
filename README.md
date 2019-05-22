d1511fd013de203612240aface1ea21f6cf09b67
LUAN FERNANDO DE SOUZA FERREIRA <luan.fernando@univille.br>
2019-05-21 21:54:58 -0300

    adicionar teste inicial

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tes_cypress_tests
 Repositório para exemplificar ferramenta de testes e2e Cypress.
+
+Ferramenta [Cypress](https://www.cypress.io)
+
---
+++ actions.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="Cypress" />
+
+context('Actions', () => {
+  beforeEach(() => {
+    cy.visit('https://univille.edu.br/access/Login.html#')
+	cy.clearCookies()
+	cy.clearLocalStorage()
+
+  })
+  
+ 
+  it('Acessar na univille e tentar logar', () => {
+    // https://on.cypress.io/type
+	
+    cy.get('#login').type('seu.nome');
+    cy.get('#password').type('digite sua senha');
+	cy.get('#userLogin').click({force:true});
+	cy.wait(50000);
+
+  })
+
+
+})
